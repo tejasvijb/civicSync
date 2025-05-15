a715538feac0b7b08419084d0702a5282d93a2e7
tejasvijb <jbtejasvi@gmail.com>
2025-05-15 10:51:39 +0530

    add aliases

--- frontend/src/app/routes/auth/login/index.tsx
+++ frontend/src/app/routes/auth/login/index.tsx
@@ -1,8 +1,7 @@
-
 import { useForm } from "react-hook-form"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../../../../components/ui/form"
-import { Input } from "../../../../components/ui/input"
-import { Button } from "../../../../components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 import { Link, useNavigate } from "react-router-dom"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { loginSchema } from "../../../utils/validations"
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@/": path.join(__dirname, "src/"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.join(__dirname, "src/"),
+            "@components": path.join(__dirname, "src/components/"),
+            "@api": path.join(__dirname, "src/api/"),
+            "@app": path.join(__dirname, "src/app/"),
+        },
     },
-  },
-})
+});
