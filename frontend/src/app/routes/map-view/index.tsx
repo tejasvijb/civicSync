2ac64db4c121f8b782ee795b82325eafb00525f7
tejasvijb <jbtejasvi@gmail.com>
2025-05-21 18:31:40 +0530

    fix marker

--- frontend/src/app/routes/map-view/index.tsx
+++ frontend/src/app/routes/map-view/index.tsx
@@ -1,6 +1,23 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { LatLngTuple } from 'leaflet';
+import { LatLngTuple, Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+// Fix for default marker icons
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Set up the default icon for markers
+const defaultIcon = new Icon({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41],
+    iconSize: [25, 41]
+});
 
 const position: LatLngTuple = [51.505, -0.09];
 
@@ -9,7 +26,6 @@ export default function MapView() {
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">Map View</h1>
             <div className="w-full h-[600px] rounded-lg shadow">
-                {/* Add your map component here */}
                 <MapContainer
                     center={position}
                     zoom={13}
@@ -20,7 +36,7 @@ export default function MapView() {
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}>
+                    <Marker position={position} icon={defaultIcon}>
                         <Popup>
                             A pretty CSS3 popup. <br /> Easily customizable.
                         </Popup>
